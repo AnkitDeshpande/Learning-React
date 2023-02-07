b3c03f05961aa7e63068a326625e4c3a2c85f27b
Ankitdeshpande <urstrulyankit@protonmail.com>
2023-02-08 01:32:12 +0530

    7-feb-2023

---
+++ Chapter 4/index.js
@@ -0,0 +1,10 @@
+ReactDOM.render(
+  <h1 className="head">Hello, I am Declarative</h1>,
+  document.getElementById("root")
+);
+
+// const h1 = document.createElement("h1");
+// h1.textContent = "Hello Ankit";
+// h1.className = "child";
+// const root = document.getElementById("root");
+// root.append(h1);
